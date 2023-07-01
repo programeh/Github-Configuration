b183d676fad66bc9b48c4d07687345fe141ea3b5
ASUTOSH GHANTO <44160439+asutoshgha@users.noreply.github.com>
2023-07-01 12:48:17 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,6 @@
 variable "github_token" {
   description = "GitHub access token used to configure the provider"
   type        = string
-  default     = "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"
   sensitive   = true
 }
 
