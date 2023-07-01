17e74bdfa94457d5dd79ff6baad865f357966956
ASUTOSH GHANTO <44160439+asutoshgha@users.noreply.github.com>
2023-07-01 21:21:33 +0530

    git: add my bulshit. (#4)
    Co-authored-by: Asutosh Ghanto <asutosh@clevertap.com>

--- GithubTerraform.tf
+++ GithubTerraform.tf
@@ -19,3 +19,42 @@ resource "github_repository" "Github_Configuration" {
   squash_merge_commit_title   = "PR_TITLE"
   vulnerability_alerts        = false
 }
+
+resource "github_branch_protection" "Github_Configuration_BP" {
+  allows_deletions                = false
+  allows_force_pushes             = true
+  blocks_creations                = false
+  enforce_admins                  = true
+  lock_branch                     = false
+  pattern                         = "master"
+  push_restrictions               = []
+  repository_id                   = github_repository.Github_Configuration.node_id
+  require_conversation_resolution = false
+  require_signed_commits          = false
+  required_linear_history         = false
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = true
+    dismissal_restrictions          = []
+    pull_request_bypassers          = []
+    require_code_owner_reviews      = false
+    require_last_push_approval      = false
+    required_approving_review_count = 0
+    restrict_dismissals             = false
+  }
+
+  required_status_checks {
+    contexts = []
+    strict   = true
+  }
+}
+
+resource "github_team" "CodeCheckers" {
+  name = "defaultCodeCheckers"
+}
+
+resource "github_team_repository" "CodecheckerTeam" {
+  team_id    = github_team.CodeCheckers.id
+  repository = github_repository.Github_Configuration.name
+  permission = "admin"
+}
