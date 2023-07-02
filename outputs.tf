af273ea7ce1955125fe37a5dccd6458de412120f
Asutosh Ghanto <asutosh@clevertap.com>
2023-07-02 17:03:17 +0530

    git: add new repo for aws infra terraform.

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "internal_repository" {
   description = "Example repository JSON blob"
   value       = github_repository.example.html_url
 }
+
+output "githubrepository" {
+  description="Github Configuration Url"
+  value = github_repository.Github_Configuration.html_url
+}
